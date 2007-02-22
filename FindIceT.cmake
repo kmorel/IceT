d19d7d99229a86282819c76c1a95f9b9d6540b04
Ken Moreland <kmorel@sandia.gov>
2007-02-22 13:25:27 -0500

    ENH: Make specifying libraries in external CMake builds easier.
    COMP: Fix a problem where turning off BUILD_TESTING did not actually turn of the build of the tests.

--- FindIceT.cmake
+++ FindIceT.cmake
@@ -8,26 +8,33 @@
 #
 # Id
 #
-# Find an ICE-T installation or build tree.
+# Find an IceT installation or build tree.
 #
-# The following variables are set if ICE-T is found.  If ICE-T is not found,
+# The following variables are set if IceT is found.  If IceT is not found,
 # ICET_FOUND is set to false.
 #
-# ICET_FOUND		- Set to true when ICE-T is found.
-# ICET_USE_FILE		- CMake source file to setup a project to use ICE-T
-# ICET_MAJOR_VERSION	- The ICE-T major version number.
-# ICET_MINOR_VERSION	- The ICE-T minor version number.
-# ICET_PATCH_VERSION	- The ICE-T patch version number.
-# ICET_INCLUDE_DIRS	- Include directories for ICE-T headers.
-# ICET_LIBRARY_DIRS	- Link directories for ICE-T libraries.
+# ICET_FOUND            - Set to true when IceT is found.
+# ICET_USE_FILE         - CMake source file to setup a project to use IceT
+# ICET_CORE_LIBS        - List of core IceT libraries that all IceT apps need.
+# ICET_MPI_LIBS         - List of libraries for using IceT with MPI (not
+#                         including MPI itself).
+# ICET_MAJOR_VERSION    - The IceT major version number.
+# ICET_MINOR_VERSION    - The IceT minor version number.
+# ICET_PATCH_VERSION    - The IceT patch version number.
 #
-# The following cache entries must be set by the user to locate ICE-T:
+# The following variables are also set, but their use is handled for you
+# when including ICET_USE_FILE.
 #
-# ICET_DIR		- The directory containing ICETConfig.cmake.
+# ICET_INCLUDE_DIRS     - Include directories for IceT headers.
+# ICET_LIBRARY_DIRS     - Link directories for IceT libraries.
+#
+# The following cache entries must be set by the user to locate IceT:
+#
+# ICET_DIR              - The directory containing ICETConfig.cmake.
 #
 
 SET(ICET_DIR_DESCRIPTION "directory containing ICETConfig.cmake.  This is either the root of the build tree, or PREFIX/lib for an installation.")
-SET(ICET_DIR_MESSAGE "ICE-T not found.  Set ICET_DIR to the ${ICET_DIR_DESCRIPTION}")
+SET(ICET_DIR_MESSAGE "IceT not found.  Set ICET_DIR to the ${ICET_DIR_DESCRIPTION}")
 
 IF (NOT ICET_DIR)
   # Get the system search path as a list.
@@ -45,39 +52,39 @@ IF (NOT ICET_DIR)
   ENDFOREACH(dir)
 
   FIND_PATH(ICET_DIR ICETConfig.cmake
-	# Look in places relative to the system executable search path.
-	${ICET_DIR_SEARCH}
-	# Look in standard UNIX install locations.
-	/usr/local/lib
-	/usr/lib
-	# Look in standard Win32 install locations.
-	"C:/Program Files/ICE-T/lib"
-	# Give documentation to user in case we can't find it.
-	DOC "The ${ICET_DIR_DESCRIPTION}")
+        # Look in places relative to the system executable search path.
+        ${ICET_DIR_SEARCH}
+        # Look in standard UNIX install locations.
+        /usr/local/lib
+        /usr/lib
+        # Look in standard Win32 install locations.
+        "C:/Program Files/IceT/lib"
+        # Give documentation to user in case we can't find it.
+        DOC "The ${ICET_DIR_DESCRIPTION}")
 ENDIF (NOT ICET_DIR)
 
-# If ICE-T was found, load the configuration file to get the rest of the
+# If IceT was found, load the configuration file to get the rest of the
 # settings.
 IF(ICET_DIR)
   # Make sure the ICETConfig.cmake file exists in the directory provided.
   IF(EXISTS ${ICET_DIR}/ICETConfig.cmake)
 
-    # We found ICE-T.  Load the settings.
+    # We found IceT.  Load the settings.
     SET(ICET_FOUND 1)
     INCLUDE(${ICET_DIR}/ICETConfig.cmake)
 
   ELSE(EXISTS ${ICET_DIR}/ICETConfig.cmake)
-    # We did not find ICE-T.
+    # We did not find IceT.
     SET(ICET_FOUND 0)
   ENDIF(EXISTS ${ICET_DIR}/ICETConfig.cmake)
 ELSE(ICET_DIR)
-  # We did not find ICE-T.
+  # We did not find IceT.
   SET(ICET_FOUND 0)
 ENDIF(ICET_DIR)
 
 #-----------------------------------------------------------------------------
 IF(NOT ICET_FOUND)
-  # ICE-T not found, explain to the user how to specify its location.
+  # IceT not found, explain to the user how to specify its location.
   IF(NOT ICET_FIND_QUIETLY)
     MESSAGE(${ICET_DIR_MESSAGE})
   ENDIF(NOT ICET_FIND_QUIETLY)
